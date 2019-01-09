5ed7a1d0b46bfc6c094e199709ff4986d8a1db70
Haaxor1689 <betkomaros@gmail.com>
2019-01-09 15:36:09 +0100

    Inverted wrong if.

--- src/Helpers/ImageUrlHelper.ts
+++ src/Helpers/ImageUrlHelper.ts
@@ -1,6 +1,6 @@
 
 export const getUrlPrefix = (): string => {
-    if (location.hostname !== "localhost") {
+    if (location.hostname === "localhost") {
         return "";
     }
     return "https://raw.githubusercontent.com/Haaxor1689/DreamQuest/master";
