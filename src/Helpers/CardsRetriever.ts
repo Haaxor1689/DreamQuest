77bf3989f537789ea6634480ecd7de3b0aca30b6
Haaxor1689 <betkomaros@gmail.com>
2019-01-09 01:23:07 +0100

    Removed console log.

--- src/Helpers/CardsRetriever.ts
+++ src/Helpers/CardsRetriever.ts
@@ -2,8 +2,4 @@ import $ from "jquery"
 
 import { Card } from 'src/Models/Cards';
 
-export const getCards = async (): Promise<Card[]> => {
-    var response = await $.getJSON("https://raw.githubusercontent.com/Haaxor1689/DreamQuest/master/data/cards.json");
-    console.log(response);
-    return response
-}
+export const getCards = async (): Promise<Card[]> => await $.getJSON("https://raw.githubusercontent.com/Haaxor1689/DreamQuest/master/data/cards.json");
