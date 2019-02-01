efbf927d2b3d26623ca98626661ea63b3da5b50c
Haaxor1689 <betkomaros@gmail.com>
2019-02-01 21:15:39 +0100

    Updates

---
+++ precache-manifest.6bfe9f3d38785d5a9a294e289ef49ecd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b605959ec3b9a437fec54ea70bb6747a",
+    "url": "/DreamQuest/static/media/TileDream.b605959e.png"
+  },
+  {
+    "revision": "c75a8dd65a949ae0ece1",
+    "url": "/DreamQuest/static/css/main.e60a102b.chunk.css"
+  },
+  {
+    "revision": "dbcb628fe21d63143138",
+    "url": "/DreamQuest/static/js/1.dbcb628f.chunk.js"
+  },
+  {
+    "revision": "77dae3128fa1fb7f5511",
+    "url": "/DreamQuest/static/js/runtime~main.77dae312.js"
+  },
+  {
+    "revision": "4a2374774758fb00b9e8c05e63ed267a",
+    "url": "/DreamQuest/static/media/Divider.4a237477.png"
+  },
+  {
+    "revision": "48d9ff8597c6a8e7b764c6dc4fed8ddc",
+    "url": "/DreamQuest/static/media/CardDescription.48d9ff85.png"
+  },
+  {
+    "revision": "0a3e65107b9ba5dc55a25de21aee0aa3",
+    "url": "/DreamQuest/static/media/CharacterDescription.0a3e6510.png"
+  },
+  {
+    "revision": "4dcf413eea7032c01cb9f3fd45a78b7c",
+    "url": "/DreamQuest/static/media/ClockDescription.4dcf413e.png"
+  },
+  {
+    "revision": "a027208ce311019e0dbef01c2ad5c41f",
+    "url": "/DreamQuest/static/media/TileBlessed.a027208c.png"
+  },
+  {
+    "revision": "5f035022badcd811ce933f9ccfd5cf07",
+    "url": "/DreamQuest/static/media/TileCursed.5f035022.png"
+  },
+  {
+    "revision": "c75a8dd65a949ae0ece1",
+    "url": "/DreamQuest/static/js/main.c75a8dd6.chunk.js"
+  },
+  {
+    "revision": "cc974d7d3085f6fdba14c2ffb3a53559",
+    "url": "/DreamQuest/static/media/TileMystery.cc974d7d.png"
+  },
+  {
+    "revision": "7380523f8c9d488277ec31640f321114",
+    "url": "/DreamQuest/static/media/TileShortcut.7380523f.png"
+  },
+  {
+    "revision": "498d7e69798cd5c5eed8b39e7bb2f065",
+    "url": "/DreamQuest/static/media/Gameboard1.498d7e69.png"
+  },
+  {
+    "revision": "108f485ab78d7a81db3d9198aaf5f332",
+    "url": "/DreamQuest/static/media/Gameboard2.108f485a.png"
+  },
+  {
+    "revision": "7073fd1d0e2605a404e9c1e18557c1dd",
+    "url": "/DreamQuest/static/media/Gameboard3.7073fd1d.png"
+  },
+  {
+    "revision": "77b69c52d565b9b151c34551676c0d0c",
+    "url": "/DreamQuest/static/media/Gameboard4.77b69c52.png"
+  },
+  {
+    "revision": "53f78ea871eedf35f9075c8ef0b7d811",
+    "url": "/DreamQuest/static/media/Gameboard.53f78ea8.png"
+  },
+  {
+    "revision": "fcc016632fa60f1c399dbd335fea7491",
+    "url": "/DreamQuest/static/media/JMHCthulhumbusArcadeUGalt1.fcc01663.woff"
+  },
+  {
+    "revision": "dc55d1190db817cfc41d2432abccb680",
+    "url": "/DreamQuest/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DreamQuest/precache-manifest.19f0b7947c8a9b1fbe9f0443c12a755f.js"
+  "/DreamQuest/precache-manifest.6bfe9f3d38785d5a9a294e289ef49ecd.js"
 );
 
 workbox.clientsClaim();
